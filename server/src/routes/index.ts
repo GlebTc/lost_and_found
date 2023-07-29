133a7b3714c0dd7eefb97b3ea8a067348876caea
GlebTc <bob88tikhonov@gmail.com>
2023-07-29 15:15:27 -0400

    Added Registration Validators
    1. The following registration validators were added to registrationValidators.ts:
      emailFormatValidator,
      emailExistsValidator,
      passwordComplexityValidator,
    
    2. A middleware to catch and return errors was added to validationsMiddleware.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,23 @@
-import { Router, Request, Response } from 'express';
-import { getUsers, registerUser } from "../controllers/userControllers"
+import { Router } from "express";
+import { getUsers, registerUser } from "../controllers/userControllers";
+import {
+  emailFormatValidator,
+  emailExistsValidator,
+  passwordComplexityValidator,
+} from "../validators/registrationValidators.";
+import { validationsMiddleware } from "../middlewares/validationsMiddleware";
 
 const router: Router = Router();
 
 // Test Route
-router.get('/test', getUsers);
-router.post('/register', registerUser)
+router.get("/test", getUsers);
+router.post(
+  "/register",
+  emailFormatValidator,
+  emailExistsValidator,
+  passwordComplexityValidator,
+  validationsMiddleware,
+  registerUser
+);
 
 module.exports = router;
