168abb2cc342a4d65cea166b5323144c713711e0
GlebTc <bob88tikhonov@gmail.com>
2023-07-30 20:29:54 -0400

    React Router setup Complete

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+import Navbar from "./Navbar";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <div className="container">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const userStatus: String = useSelector((state: any) => state.user.userStatus);
+  console.log(userStatus);
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <div>
+          <NavLink to="/">
+            <span className="navbar-brand mb-0 h1">Home</span>
+          </NavLink>
+        </div>
+
+        <div>
+          <NavLink to="/dashboard" className="mx-3">
+            <span>Dashboard</span>
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/login">
+            <span>Login</span>
+          </NavLink>
+
+          <NavLink to="/register" className="mx-3">
+            <span>Register</span>
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
