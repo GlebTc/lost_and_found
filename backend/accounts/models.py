3ff51917f60078894f0c823e4738ff8601bf64a3
GlebTc <bob88tikhonov@gmail.com>
2025-04-26 23:19:20 -0400

    Updated Registration and Login functionality
    1.  Modified to use supabase SDK for authentication
    2.  Added Profil model to store user details other than authentication information.
    3.  Re-created regsitration and login views to properly utilize auth and profile tables.
    
    Q.A.
    1.  Use /api/accounts/register/ end point to register a new user.  Response should be a success message.
    2.  Use /api/accounts/login/ end point to login a user (use raw json data).  The response should be success and include an access_token and profile information.

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=50, default='user')
+    first_name = models.CharField(max_length=250, blank=True, null=True)
+    last_name = models.CharField(max_length=250, blank=True, null=True)
+    avatar_url = (models.URLField(blank=True, null=True))
+    
+    def __str__(self):
+        return f"{self.email} ({self.role})"
