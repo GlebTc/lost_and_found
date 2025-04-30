576d74a0e4b2884f41afbfdafacb85edb5dbc5ca
GlebTc <bob88tikhonov@gmail.com>
2025-04-30 00:36:35 -0400

    API for admin to patch user by id
    1.  Created view that checks requestor role
    2.  If rols is admin, allows the requestor to modify user information by user_id
    
    Q.A.
    Hit API http://localhost:8000/api/v1/admin/users/user_id/update/
    Use JWT as "Authorization", provide updates in request body
    
    Should receive a successful response with updated user information.

--- backend/accounts/urls/admin_urls.py
+++ backend/accounts/urls/admin_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from accounts.views.admin_views import get_all_users, get_individual_user_info
+from accounts.views.admin_views import get_all_users, get_individual_user_info, update_user
 
 urlpatterns = [
     path('users/', get_all_users, name='get_all_users'),
     path('users/<str:user_id>/', get_individual_user_info, name='get_individual_user_info'),
+    path('users/<str:user_id>/update/', update_user, name='update_user'),
+
 ]
