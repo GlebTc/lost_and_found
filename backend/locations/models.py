6f6e6d87ad229560f58c7c428c9cccfba87a0e02
GlebTc <bob88tikhonov@gmail.com>
2025-05-01 01:37:38 -0400

    Completed locations setup
    This was a setup only.  I have created 4 tables that separate locations into 4 categories: location, building, level, department.  I have created models but am leaving this for now and not creating any views or urls until I am ready to use it.

--- backend/locations/models.py
+++ backend/locations/models.py
@@ -8,7 +8,7 @@ class Building(models.Model):
     location = models.ForeignKey(Location, related_name="buildings", on_delete=models.CASCADE)
 
 class Level(models.Model):
-    number = models.CharField(max_length=25)  # or CharField if you want L1, B1, etc.
+    number = models.CharField(max_length=25)  # Using CharFiled as level may include basements, etc.
     building = models.ForeignKey(Building, related_name="levels", on_delete=models.CASCADE)
 
 class Department(models.Model):
