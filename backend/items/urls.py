4f4a92cf225190f898ae51953bd4b0a5e5b21ca4
GlebTc <bob88tikhonov@gmail.com>
2025-05-04 06:31:15 -0400

    Added GET, PATCH, DELETE for items

--- backend/items/urls.py
+++ backend/items/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import create_get_all_items
+from .views import create_get_all_items, get_patch_delete_item
 
 urlpatterns = [
     path('items/', create_get_all_items, name='create_get_all_items'),
+    path('items/<str:item_id>/', get_patch_delete_item, name="get_patch_delete_item")
 ]
