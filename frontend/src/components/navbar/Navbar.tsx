c0eab53f840ad5711b853845c110b7590bc5f4e6
GlebTc <bob88tikhonov@gmail.com>
2025-05-10 07:38:46 -0400

    Added Dropdown menu
    1.  Added Dropdown menu to the user avatar
    2.  Found and fixed error, (protected)/layou.tsx changed to /layout.tsx

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,21 +1,21 @@
 'use client';
-import Link from 'next/link';
 import { useState } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import hhs_logo from '@/public/images/logo/hhs_logo.webp';
 import { useAuth } from '@/src/contexts/AuthContext';
+import AvatarDropdown from './AvatarDropdown';
 
 const Navbar = () => {
   const componentName = 'NAVBAR';
   const { isAuthenticated, profile, logout } = useAuth();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  console.log(`Authentication: ${isAuthenticated}`)
 
   return (
     <nav className='bg-gray-100 fixed w-full h-[var(--navbar-h)] flex justify-between items-center px-4 md:px-20 border-b-1 border-gray-500'>
-      {/* Left: Logo */}
-      <Link
-        href='/'
-        className='flex items-center gap-2 rounded-xl'
-      >
+      <Link href='/' className='flex items-center gap-2 rounded-xl'>
         <Image
           src={hhs_logo}
           alt='Hamilton Health Sciences'
@@ -24,24 +24,31 @@ const Navbar = () => {
           className='h-10 w-auto rounded-xl'
         />
       </Link>
-      <div
-        className={`${componentName}_CONTENT_CONTAINER w-fit flex items-center justify-end gap-8`}
-      >
-        {/* Avatar - optional */}
-        {isAuthenticated &&
-          (profile?.avatar_url ? (
-            <img
-              src={profile.avatar_url}
-              alt='User Avatar'
-              className='w-14 h-14 rounded-full object-cover border border-gray-400'
-            />
-          ) : (
-            <div className='w-10 h-10 rounded-full bg-cyan-700 hover:bg-cyan-600 text-white flex items-center justify-center text-xl font-bold transition duration-[var(--duration)] cursor-pointer'>
-              {profile?.first_name
-                ? profile.first_name[0].toUpperCase()
-                : profile?.email[0].toUpperCase()}
+
+      <div className={`${componentName}_CONTENT_CONTAINER w-fit flex items-center justify-end gap-8`}>
+        {/* Avatar and Dropdown */}
+        {isAuthenticated && (
+          <div className='AVATAR_CONTAINER relative'>
+            <div onClick={() => setMenuOpen((prev) => !prev)} className='cursor-pointer'>
+              {profile?.avatar_url ? (
+                <img
+                  src={profile.avatar_url}
+                  alt='User Avatar'
+                  className='w-14 h-14 rounded-full object-cover border border-gray-400'
+                />
+              ) : (
+                <div className='w-10 h-10 rounded-full bg-cyan-700 hover:bg-cyan-600 text-white flex items-center justify-center text-xl font-bold transition duration-[var(--duration)]'>
+                  {profile?.first_name
+                    ? profile.first_name[0].toUpperCase()
+                    : profile?.email[0].toUpperCase()}
+                </div>
+              )}
             </div>
-          ))}
+            <AvatarDropdown menuOpen={menuOpen} />
+          </div>
+        )}
+
+        {/* Login/Logout Button */}
         {isAuthenticated ? (
           <button
             onClick={logout}
